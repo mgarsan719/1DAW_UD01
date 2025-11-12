7433591f3f8230cf375a80cf3d74c91cf76212d9
mgs0077 <166002593+mgs0077@users.noreply.github.com>
2025-11-12 15:48:11 +0100

    ejercicios en casa

---
+++ ExplorandoEclipse/src/Entornos/Principal.java
@@ -0,0 +1,13 @@
+package Entornos;
+
+public class Principal
+{
+	public static void main(String[] args)
+	{
+		// He puesto el tema claro y he cambiado la fuente de texto al editor de coódigo
+		// de java
+
+		System.out.println("Bienvenido a Eclipse");
+		System.out.println("Vamos a conocer el entorno de desarrollo");
+	}
+}
